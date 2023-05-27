317fb616fe699f958acf26d25817332fe2e42614
Emmanuel-002 <segseyman8@gmail.com>
2023-05-27 20:55:33 +0100

    final commit

--- start/src/MessageBox.js
+++ start/src/MessageBox.js
@@ -1,8 +1,6 @@
 import React, {useState} from "react";
 
 const sentenceCase = str => {
-  if (str === null || str === "") return false;
-  else str = str.toString();
 
   return str.replace(/\w\S*/g, function(txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
